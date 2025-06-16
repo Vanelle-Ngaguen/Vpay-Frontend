4ee4eda8591d335328e5e8e79beaeb1513fcba36
Minister <mundestephane13@gmail.com>
2025-06-16 17:18:05 +0100

    fixed little UI issues

---
+++ app/(auth)/password-reset.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const PasswordReset = () => {
+	return <Text>hello from the password reset screen</Text>;
+};
+
+export default PasswordReset;
---
+++ app/(auth)/transaction.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const Transaction = () => {
+	return <Text>Hello from the transaction screen</Text>;
+};
+
+export default Transaction;
