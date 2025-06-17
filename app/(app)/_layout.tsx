83b25001ecb4a1057a4ba15ee4e737806404377c
Minister <mundestephane13@gmail.com>
2025-06-17 16:07:24 +0100

    Fixed header style for app layout

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -2,8 +2,15 @@ import { Stack } from "expo-router";
 
 const AppLayout = () => {
 	return (
-		<Stack screenOptions={{ headerShown: true }}>
-			<Stack.Screen name="index" />
+		<Stack
+			screenOptions={{
+				headerShown: true,
+				headerStyle: { backgroundColor: "#580097" },
+				headerTintColor: "#fff",
+			}}
+		>
+			<Stack.Screen name="index" options={{ headerShown: false }} />
+			<Stack.Screen name="cards" />
 		</Stack>
 	);
 };
